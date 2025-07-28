f575a7045cce00c62bea0798b484e254521f3c83
jjayaraaj <jjayaraaj@gmail.com>
2025-07-28 20:47:22 +0530

    Dummy: Test

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,7 @@ const nextConfig = {
     images: {
         remotePatterns: [
           // Your existing patterns here...
-          {
-            protocol: 'https',
-            hostname: 'img.clerk.com',
-          },
-          {
-            protocol: 'https',
-            hostname: 'images.clerk.dev',
-          },
+        
         ],
       },
       
