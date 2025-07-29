ccc1233581a95eb2ee22fea5e63dcc290a0f1fd8
Gianni James Favaretto <giannijfavaretto@gmail.com>
2025-07-29 10:51:45 +0100

    GA4 implementation 29/07/25

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,18 @@ export default function RootLayout({
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<head>
+				{/* Google Tag Manager */}
+				<script 
+					dangerouslySetInnerHTML={{
+						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+						})(window,document,'script','dataLayer','GTM-5X8NMTFR');`
+					}}
+				/>
+				{/* End Google Tag Manager */}
+				
 				{/* Preload critical font variants for optimal performance */}
 				<link
 					rel="preload"
@@ -49,8 +61,20 @@ export default function RootLayout({
 				/>
 				{/* DNS prefetch for better performance */}
 				<link rel="dns-prefetch" href="//fonts.googleapis.com" />
+				<link rel="dns-prefetch" href="//www.googletagmanager.com" />
 			</head>
 			<body className="antialiased">
+				{/* Google Tag Manager (noscript) */}
+				<noscript>
+					<iframe 
+						src="https://www.googletagmanager.com/ns.html?id=GTM-5X8NMTFR"
+						height="0" 
+						width="0" 
+						style={{display: 'none', visibility: 'hidden'}}
+					/>
+				</noscript>
+				{/* End Google Tag Manager (noscript) */}
+				
 				<ThemeProvider
 					attribute="class"
 					defaultTheme="system"
